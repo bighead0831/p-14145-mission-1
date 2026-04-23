344859f4297b4aaa71451a4c63bcd75f031c7fc5
bighead0831 <bighead0831@naver.com>
2026-04-24 01:13:12 +0900

    6~7단계 구현 - 삭제, 예외처리

--- p-14145-1/src/main/java/com/back/Main.java
+++ p-14145-1/src/main/java/com/back/Main.java
@@ -18,10 +18,10 @@ public class Main {
         System.out.println("== 명언 앱 ==");
         while(true) {
             System.out.print("명령) ");
-            String input = sc.nextLine();
-            if(input.equals("종료"))
+            String cmd = sc.nextLine();
+            if(cmd.equals("종료")) {
                 break;
-            else if(input.equals("등록")) {
+            } else if(cmd.equals("등록")) {
                 WiseSaying ws = new WiseSaying();
 
                 wsLastId++; // 등록하면, ws배열에 하나 추가
@@ -33,13 +33,35 @@ public class Main {
                 wss[ws.id-1] = ws;
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", ws.id);
-            } else if(input.equals("목록")) {
+            } else if(cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언\n----------------------");
                 for(int i=wsLastId; i>=0; i--) {
                     System.out.printf("%d / %s / %s\n", wss[i].id, wss[i].content, wss[i].author);
                 }
+            } else if(cmd.equals("삭제")) {
+                System.out.print("?id=");
+                int deleteId = sc.nextInt();
+
+                int deleteRealId = -1; // 삭제할 id의 배열에서의 실제 인덱스
+                for(int i=0; i<=wsLastId; i++) { // deleteId와 같은 wss배열의 값의 id가 있는지 확인
+                    if(wss[i].id == deleteId) {
+                        deleteRealId = i;
+                        break;
+                    }
+                }
+
+                if(deleteRealId==-1)
+                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", deleteId);
+                else {
+                    for(int i=deleteRealId; i<wsLastId; i++) {
+                        wss[i] = wss[i+1];
+                    }
+                    wss[wsLastId] = null;
+                    wsLastId--;
+
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", deleteId);
+                }
             }
         }
-
     }
 }
