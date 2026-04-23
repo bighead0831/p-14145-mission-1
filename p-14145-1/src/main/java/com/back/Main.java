16d2fa98c9a492684b3de45b180399656fc7310f
bighead0831 <bighead0831@naver.com>
2026-04-24 00:46:03 +0900

    3단계 구현 - 등록시 생성된 명언번호 노출

--- p-14145-1/src/main/java/com/back/Main.java
+++ p-14145-1/src/main/java/com/back/Main.java
@@ -31,6 +31,8 @@ public class Main {
                 System.out.print("작가 : ");
                 ws.author = sc.nextLine();
                 wss[ws.id-1] = ws;
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", ws.id);
             }
         }
 
