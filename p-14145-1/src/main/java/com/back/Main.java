8badc4fab19482fa1e8125f148b6c9b257a03ef4
bighead0831 <bighead0831@naver.com>
2026-04-24 00:29:56 +0900

    1단계 구현 - 종료

--- p-14145-1/src/main/java/com/back/Main.java
+++ p-14145-1/src/main/java/com/back/Main.java
@@ -1,7 +1,18 @@
 package com.back;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        while(true) {
+            System.out.print("명언) ");
+            String input = sc.nextLine();
+            if(input.equals("종료"))
+                break;
+        }
+
     }
 }
