76b39c8141c2d51cbb4ae665d66ea0a9ec428c9f
bighead0831 <bighead0831@naver.com>
2026-04-24 20:00:37 +0900

    8단계까지 구현 - 명언(ver. 리팩토링 미실시)

--- p-14145-1/src/main/java/com/back/Main.java
+++ p-14145-1/src/main/java/com/back/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         WiseSaying[] wss = new WiseSaying[10000]; // 일단 10,000개 받을 수 있게 설정.
         int wsLastId = -1; // ws배열에 아무것도 안 들어있으므로, 인덱스 -1로 초기화
+        int cntId = -1;
 
         System.out.println("== 명언 앱 ==");
         while(true) {
@@ -25,12 +26,12 @@ public class Main {
                 WiseSaying ws = new WiseSaying();
 
                 wsLastId++; // 등록하면, ws배열에 하나 추가
-                ws.id = wsLastId+1;
+                ws.id = ++cntId+1;
                 System.out.print("명언 : ");
                 ws.content = sc.nextLine();
                 System.out.print("작가 : ");
                 ws.author = sc.nextLine();
-                wss[ws.id-1] = ws;
+                wss[wsLastId] = ws;
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", ws.id);
             } else if(cmd.equals("목록")) {
@@ -38,9 +39,14 @@ public class Main {
                 for(int i=wsLastId; i>=0; i--) {
                     System.out.printf("%d / %s / %s\n", wss[i].id, wss[i].content, wss[i].author);
                 }
-            } else if(cmd.equals("삭제")) {
-                System.out.print("?id=");
-                int deleteId = sc.nextInt();
+            } else if(cmd.startsWith("삭제")) {
+                String[] cmdBits = cmd.split("="); // cmdBits: [삭제?id=][index]
+                if(cmdBits.length < 2 || !cmdBits[0].equals("삭제?id")) {
+                    System.out.println("\'삭제\'명령을 정확히 작성해주시기 바랍니다.");
+                    continue;
+                }
+
+                int deleteId = Integer.parseInt(cmdBits[1]);
 
                 int deleteRealId = -1; // 삭제할 id의 배열에서의 실제 인덱스
                 for(int i=0; i<=wsLastId; i++) { // deleteId와 같은 wss배열의 값의 id가 있는지 확인
@@ -56,10 +62,34 @@ public class Main {
                     for(int i=deleteRealId; i<wsLastId; i++) {
                         wss[i] = wss[i+1];
                     }
-                    wss[wsLastId] = null;
-                    wsLastId--;
+                    wss[wsLastId--] = null;
 
                     System.out.printf("%d번 명언이 삭제되었습니다.\n", deleteId);
+                }
+            } else if(cmd.startsWith("수정")) {
+                String[] cmdBits = cmd.split("="); // cmdBits: [삭제?id=][index]
+                if(cmdBits.length < 2 || !cmdBits[0].equals("수정?id")) {
+                    System.out.println("\'수정\'명령을 정확히 작성해주시기 바랍니다.");
+                    continue;
+                }
+                int modifyId = Integer.parseInt(cmdBits[1]);
+
+                int modifyRealId = -1; // 수정할 id의 배열에서의 실제 인덱스
+
+                for(int i=0; i<=wsLastId; i++) { // modifyId와 같은 wss배열의 값의 id가 있는지 확인
+                    if(wss[i].id == modifyId) {
+                        modifyRealId = i;
+                        break;
+                    }
+                }
+
+                if(modifyRealId==-1)
+                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", modifyId);
+                else {
+                    System.out.printf("명언(기존) : %s\n명언 : ", wss[modifyRealId].content);
+                    wss[modifyRealId].content = sc.nextLine();
+                    System.out.printf("작가(기존) : %s\n작가 : ", wss[modifyRealId].author);
+                    wss[modifyRealId].author = sc.nextLine();
                 }
             }
         }
